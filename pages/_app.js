afcb0ffab654d85f3e7ecadcc13d9adbdee35a19
Alexandre Desroches <alex.desroches7@gmail.com>
2022-08-03 17:23:57 -0400

    Improved gtag loading with Next's Script component.

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,8 @@
+import {useEffect} from "react";
 import Head from "next/head";
+import {useRouter} from "next/router";
+import * as gtag from "../utils/gtag.js";
+import Script from "next/script";
 
 import "../styles/global.css";
 import "../styles/CornerRibbonComponent.css";
@@ -7,6 +11,19 @@ import "../styles/ResultsTableComponent.css";
 import "../styles/FooterComponent.css";
 
 export default function MyApp({Component, pageProps}) {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = url => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    router.events.on("hashChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+      router.events.off("hashChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <>
       <Head>
@@ -65,15 +82,23 @@ export default function MyApp({Component, pageProps}) {
           href={process.env.NEXT_PUBLIC_WEBSITE_URL + "/icons/safari-pinned-tab.svg"}
           color="#21beeb"
         />
-        {/*google analytics*/}
-        <script
-          defer
-          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+        {/* Global Site Tag (gtag.js) - Google Analytics */}
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
         />
-        <script
-          defer
+        <Script
+          id="gtag-init"
+          strategy="afterInteractive"
           dangerouslySetInnerHTML={{
-            __html: `window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');`
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `
           }}
         />
       </Head>
